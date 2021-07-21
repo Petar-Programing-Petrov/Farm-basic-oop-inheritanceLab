81a80aacaf26f670069f7b0b3d86487726a55059
Petar-Programing-Petrov <petar.dragov.petrov@gmail.com>
2021-07-21 11:52:13 +0200

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceLab
+{
+    class Employee
+    {
+        public Employee(string name, int salary)
+        {
+            this.Name = name;
+            this.Salary = salary;
+        }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+}
